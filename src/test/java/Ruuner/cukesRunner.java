0f4843fc81698a8b34ba03df4fc3c012834d6904
Shirin Davletova <shirindavletova@Shirins-MacBook-Pro-2.local>
2020-03-19 19:44:59 -0500

    created feature files

--- src/test/java/Ruuner/cukesRunner.java
+++ src/test/java/Ruuner/cukesRunner.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty","html:target\\cucumber-html-report","json:target\\cucumber.json","junit:target\\cucumber.xml"},
         features = "src\\test\\resources",
-        //C:\Users\Nurkulov\Downloads\Cucumber-Class\src\test\resources\IceHRM\DataTableMap.feature
         glue = "StepDefinitions",
         dryRun = false,
         monochrome = true,
